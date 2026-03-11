97f04fa6f284968bbc7f5373debb5b5a2f7e4ef2
Mahir1401 <mahirrahman1122@gmail.com>
2026-03-12 03:34:28 +0600

    parent

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let interviewList = [];
+let rejectedList = [];
+
 let totalCount = document.getElementById('Total-count');
 let interviewCount = document.getElementById('Interview-count');
 let rejectedCount = document.getElementById('Rejected-count');
@@ -22,8 +25,79 @@ function toggle(name) {
   allFilterBtn.classList.add('btn-soft');
   interviewFilterBtn.classList.add('btn-soft');
   rejectedFilterBtn.classList.add('btn-soft');
-  
+
   const selectedBtn = document.getElementById(name)
   selectedBtn.classList.remove('btn-soft');
   selectedBtn.classList.add('btn-primary');
 }
+
+const mainContainer = document.getElementById('main-container');
+
+mainContainer.addEventListener('click', function (event) {
+  
+  if(event.target.classList.contains('Interview-btn')){
+  const parentNode = event.target.parentNode.parentNode;
+  const name = parentNode.querySelector('.name').innerText;
+  const position = parentNode.querySelector('.position').innerText;
+  const place = parentNode.querySelector('.place').innerText;
+  const time = parentNode.querySelector('.time').innerText;
+  const salary = parentNode.querySelector('.salary').innerText;
+  const statu = parentNode.querySelector('.statu').innerText;
+  const description = parentNode.querySelector('.description').innerText;
+
+  const cardInfo = {
+    name,
+    position,
+    place,
+    time,
+    salary,
+    statu,
+    description
+  };
+
+  const interviewExist = interviewList.find(item => item.name === cardInfo.name)
+  if (!interviewExist) {
+    interviewList.push(cardInfo);
+  }
+  renderInterview()
+  }
+})
+
+function renderInterview() {
+  interviewSection.innerHTML = '';
+  let div = document.createElement('div');
+  div.classList.add("border-0", "rounded-md", "shadow", "bg-white", "p-6", "mb-7", "flex", "justify-between")
+  for (let interview of interviewList) {
+    div.innerHTML = `
+          <div>
+            <h2 class="font-bold text-xl name">
+              ${interview.name}
+            </h2>
+            <p class="text-lg position">
+              ${interview.position}
+            </p>
+            <br>
+            <ul class="flex gap-8">
+              <li class="place">${interview.place}</li>
+              <li class="list-disc time">${interview.time}</li>
+              <li class="list-disc salary">${interview.salary}</li>
+            </ul>
+            <br>
+            <div>
+              <span class="badge badge-xl font-semibold bg-green-400">Interview</span>
+            </div>
+            <br>
+            <p class="description">${interview.description}</p>
+            <br>
+            <div>
+              <button id="Interview-btn" class="btn btn-outline btn-success">Interview</button>
+              <button id="Rejected-btn" class="btn btn-outline btn-error">Rejected</button>
+            </div>
+          </div>
+          <div>
+            <button class="Delete-btn btn rounded-full"><i class="fa-regular fa-trash-can"></i></button>
+          </div>
+    `
+  }
+}
+
